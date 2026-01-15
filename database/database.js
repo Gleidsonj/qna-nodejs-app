3ca4177810ed0fd340ecee61e24d8177464fdd12
gleidson <gleidsonlimacrf@gmail.com>
2026-01-15 20:04:37 -0300

    adicionando logs de teste

--- database/database.js
+++ database/database.js
@@ -1,5 +1,11 @@
 const sequelize = require('sequelize')
 
+console.log('DB_HOST =', process.env.DB_HOST);
+console.log('DB_NAME =', process.env.DB_NAME);
+console.log('DB_USER =', process.env.DB_USER);
+console.log('DB_PASSWORD =', process.env.DB_PASSWORD);
+console.log('DB_PORT =', process.env.DB_PORT);
+
 const connection = new sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
