cc39a0cba2284fbf1830d5d1f4b5bb3683926371
gleidson <gleidsonlimacrf@gmail.com>
2026-01-15 19:32:51 -0300

    ajustando valores

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const sequelize = require('sequelize')
 const connection = new sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql'
